2296d2d4a45dd83baccff2cc37f7ced29a400480
Andres Ruiz <andrsruiz@gmail.com>
2020-01-01 15:23:48 -0600

    Add repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, number) {
+	myStr = '';
 
+	if(number != -1) {
+		for(let i = 0; i < number; i++) {
+			myStr += str;
+		}
+	}else {
+		myStr = 'ERROR';
+	}
+
+	return myStr;
 }
 
 module.exports = repeatString
