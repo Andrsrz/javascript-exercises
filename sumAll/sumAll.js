c59bc8b47641f07ef8501c6454b03983a97c23e4
Andres Ruiz <andrsruiz@gmail.com>
2020-01-01 15:26:23 -0600

    Add sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,25 @@
 const sumAll = function() {
+	const ERROR = 'ERROR';
+	const NUMBER = 'number';
+	let sum = 0;
 
+	if( ( typeof arguments[0] != NUMBER ) || ( typeof arguments[1] != NUMBER ) ){
+		return ERROR;
+	}else if( ( arguments[0] < 0 ) || ( arguments[1] < 0 ) ){
+		return ERROR;
+	}
+
+	if( arguments[0] > arguments[1] ){
+		for(let i = arguments[1]; i <= arguments[0]; i++ ){
+			sum += i;
+		}
+	}else{
+		for(let i = arguments[0]; i <= arguments[1]; i++ ){
+			sum += i;
+		}
+	}
+
+	return sum;
 }
 
 module.exports = sumAll
