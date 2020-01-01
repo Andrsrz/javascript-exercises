963052e65df37f8e013ff5134dda2f6a3aec5b58
Andres Ruiz <andrsruiz@gmail.com>
2020-01-01 15:27:02 -0600

    Add leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,21 @@
 const leapYears = function() {
+	// Avoid magic numbers
+	const FOUR = 4;
+	const HUNDRED = 100;
+	const FOUR_HUNDRED = 400;
+	let leapYear = false;
 
+	if(arguments[0] % FOUR == 0){
+		leapYear = true;
+		if(arguments[0] % HUNDRED == 0){
+			leapYear = false;
+			if(arguments[0] % FOUR_HUNDRED == 0){
+				leapYear = true;
+			}
+		}
+	}
+
+	return leapYear;
 }
 
 module.exports = leapYears
