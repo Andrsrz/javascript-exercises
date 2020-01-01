406d2cbaafd2ec201f807f5ec2f38069d0b0051e
Andres Ruiz <andrsruiz@gmail.com>
2020-01-01 15:25:43 -0600

    Add removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
+	let myArr = arguments[0];
+	let myArrSize = myArr.length;
+	let argCount = arguments.length;
 
+	for(let i = 1; i <= argCount; i++) {
+		for(let j = 0; j < myArrSize; j++){
+			if(myArr[j] == arguments[i]) {
+				myArr.splice(j, 1);
+			}
+		}
+	}
+	
+	return myArr;
 }
 
 module.exports = removeFromArray
