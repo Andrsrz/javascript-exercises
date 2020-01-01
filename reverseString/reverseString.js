5c6d874409c19d9f867a20eb8f8f05f9dc206bab
Andres Ruiz <andrsruiz@gmail.com>
2020-01-01 15:24:47 -0600

    Add reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+	let myStr = '';
+	let strSize = str.length - 1;
 
+	for(let i = strSize; i >= 0; i--) {
+		myStr += str[i];
+	}
+
+	return myStr;
 }
 
 module.exports = reverseString
