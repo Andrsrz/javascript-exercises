bd72557484baea6e09546fad0bb623bce5b0c1f0
Andres Ruiz <andrsruiz@gmail.com>
2020-04-29 13:04:45 -0500

    Add palindrome 1 word
    Test passed

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function() {
+const palindromes = function(phrase) {
+	let reversedPhrase = '';
 
+	for (let i = phrase.length - 1; i >= 0; i--){
+		reversedPhrase += phrase[i];
+	}
+
+	return phrase == reversedPhrase;
 }
 
 module.exports = palindromes
